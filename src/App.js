efa321e1d94229e6cabab950820045d2db449a23
SctBrian <ddbrayan10@hotmail.com>
2021-03-07 12:41:26 -0300

    ApolloClient was implemented in the server, now is possible to send requests to the apollo server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
-import Pages from './pages/index';
+import "./App.css";
+import Pages from "./pages/index";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache
+} from '@apollo/client';
+
+
+const uri = "http://localhost:4000/foroApi";
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
 
 function App() {
   return (
-     <Pages />
+    <ApolloProvider client={client}>
+      <Pages />
+    </ApolloProvider>
   );
 }
 
