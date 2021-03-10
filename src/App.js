6392d686b257ee3e0919dd697df42a01b870a31f
SctBrian <ddbrayan10@hotmail.com>
2021-03-10 18:12:11 -0300

    Now, the apollo client send every request to the server with an authorization header having the jwt token as value.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import Pages from "./pages/index";
 import {
   ApolloClient,
   ApolloProvider,
-  InMemoryCache
-} from '@apollo/client';
-
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 const uri = "http://localhost:4000/foroApi";
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem("token") || "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
-  connectToDevTools: true
+  resolvers: {},
+  connectToDevTools: true,
 });
 
 function App() {
